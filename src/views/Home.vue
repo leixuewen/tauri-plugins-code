cb1c846a42e1abbc9d416f4478aa68e46440c20c
lxw <lei_aa00@163.com>
2025-07-30 16:24:01 +0800

    sessionStorage save state

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
 import plugins from '@/router/plugins';
 import tauriApi from '@/router/tauri-api';
 import nativeJS from '@/router/native-js';
-import { ref } from 'vue';
+import { onBeforeMount, onMounted, ref } from 'vue';
 
 const active = ref(0);
 const tabs = ref([
@@ -11,11 +11,41 @@ const tabs = ref([
   { val: nativeJS, label: "nativeJS", icon: "code-json" },
 ]);
 
+onBeforeMount(() => {
+  if (sessionStorage.__Home_active) {
+    active.value = parseInt(sessionStorage.__Home_active);
+  }
+});
+function changeFun() {
+  sessionStorage.__Home_active = active.value;
+}
+
+onMounted(() => {
+  tabs.value.forEach(v => {
+    let top = sessionStorage['__Home_scrollTop_' + v.label];
+    if (top) {
+      // console.log(v.label, top);
+      // console.log(document.getElementsByClassName("app " + v.label)[0]);
+      document.getElementsByClassName("app " + v.label)[0].scrollTo(0, parseInt(top));
+    }
+  });
+});
+let scroll = null;
+function scrollFun(e) {
+  // console.log(e);
+  // console.log(e.target.scrollTop);
+  // console.log(e.target.classList[3]);
+  if (scroll) clearTimeout(scroll);
+  scroll = setTimeout(() => {
+    // console.log(e.target.scrollTop, e.target.classList[3]);
+    sessionStorage['__Home_scrollTop_' + e.target.classList[3]] = e.target.scrollTop;
+  }, 200);
+}
 </script>
 
 <template>
   <var-tabs-items v-model:active="active">
-    <var-tab-item class="app" v-for="tab of tabs">
+    <var-tab-item class="app" v-for="tab of tabs" :class="tab.label" @scroll="scrollFun">
       <var-cell v-for="item of tab.val" @click="$router.push(item.path)" :title="item.name"
         :description="item.meta.description" :border="true">
         <template #extra>
@@ -24,7 +54,7 @@ const tabs = ref([
       </var-cell>
     </var-tab-item>
   </var-tabs-items>
-  <var-bottom-navigation v-model:active="active">
+  <var-bottom-navigation v-model:active="active" @change="changeFun">
     <var-bottom-navigation-item v-for="tab of tabs" :label="tab.label" :icon="tab.icon" />
   </var-bottom-navigation>
 </template>
