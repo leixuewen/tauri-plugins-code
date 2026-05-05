e8085fe2943462a0b3801a935fbdecdc70d587fc
lxw <lei_aa00@163.com>
2026-05-05 20:23:52 +0800

    add Performance.vue

---
+++ src/views/native-js/Performance.vue
@@ -0,0 +1,18 @@
+<script setup>
+import {ref} from "vue";
+
+const performance = ref({
+  now: window.performance.now(),
+  navigation: window.performance.navigation,
+  timeOrigin: window.performance.timeOrigin,
+  timing: window.performance.timing,
+  getEntries: window.performance.getEntries(),
+})
+
+</script>
+
+<template>
+  <var-card title="Performance">
+    <var-card :title="item" v-for="item in Object.keys(performance)">{{ performance[item] }}</var-card>
+  </var-card>
+</template>
