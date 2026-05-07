a048a1f0b6d6dca08afbd6d42748c5b1b10cbc50
lxw <lei_aa00@163.com>
2026-05-07 17:37:38 +0800

    add Location.vue

---
+++ src/views/native-js/Location.vue
@@ -0,0 +1,14 @@
+<script setup>
+let location = {}
+Object.keys(window.location).forEach(v => {
+  if (typeof window.location[v] === 'string') {
+    location[v] = window.location[v];
+  }
+})
+</script>
+
+<template>
+  <var-card title="Location">
+    <var-cell v-for="item in Object.keys(location)" :title="item" :description="location[item]"/>
+  </var-card>
+</template>
